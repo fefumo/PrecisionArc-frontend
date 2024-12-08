7b0a56389151048637073603a2ba6b3a859bd450
fedos <fedrodj@gmail.com>
2024-12-08 14:36:53 +0300

    added PrimeReact on main page

--- src/components/forms/AddPointForm.js
+++ src/components/forms/AddPointForm.js
@@ -1,37 +1,42 @@
 import React, { useState } from 'react';
 import { useAddUserPointMutation } from '../../services/graph-api';
 import PointInput from './PointInput';
+import { Button } from "primereact/button";
 
 const AddPointForm = ({ r, onRChange }) => {
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
-  const [addUserPoint] = useAddUserPointMutation();  // Хук для отправки запроса на добавление точки
+  const [addUserPoint] = useAddUserPointMutation();  // Hook for sending a request to add a point
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const newPoint = { x, y, r };  
+    const newPoint = { x, y, r };
     console.log('Submitting new point:', newPoint);
 
     try {
-        const response = await addUserPoint(newPoint).unwrap();  
-        console.log('Server response:', response); 
+      const response = await addUserPoint(newPoint).unwrap();
+      console.log('Server response:', response);
     } catch (error) {
-        console.error('Error adding point:', error); 
+      console.error('Error adding point:', error);
     }
 
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <PointInput 
-        x={x} 
-        y={y} 
-        r={r}  // Передаем r сюда
-        onXChange={setX} 
-        onYChange={setY} 
-        onRChange={onRChange}  // Передаем функцию для изменения r
+      <PointInput
+        x={x}
+        y={y}
+        r={r} 
+        onXChange={setX}
+        onYChange={setY}
+        onRChange={onRChange}  
       />
-      <button type="submit">Add Point</button>
+      <Button
+        label="Add Point"
+        icon="pi pi-check"
+        type="submit"
+      />
     </form>
   );
 };
--- src/components/forms/PointInput.js
+++ src/components/forms/PointInput.js
@@ -1,32 +1,50 @@
-import React from 'react';
+import React from "react";
+import { Slider } from "primereact/slider";
+import { InputText } from "primereact/inputtext";
+import "primereact/resources/primereact.min.css";
+import "primereact/resources/themes/saga-blue/theme.css";
+import "primeicons/primeicons.css";
 
-const PointInput = ({ x, y, r, onXChange, onYChange, onRChange }) => (
-  <div>
-    <label>
-      X:
-      <input 
-        type="number" 
-        value={x} 
-        onChange={(e) => onXChange(Number(e.target.value))} 
-      />
-    </label>
-    <label>
-      Y:
-      <input 
-        type="number" 
-        value={y} 
-        onChange={(e) => onYChange(Number(e.target.value))} 
-      />
-    </label>
-    <label>
-      R:
-      <input 
-        type="number" 
-        value={r} 
-        onChange={(e) => onRChange(Number(e.target.value))}  // Вызываем onRChange для обновления значения R
-      />
-    </label>
-  </div>
-);
+const PointInput = ({ x, y, r, onXChange, onYChange, onRChange }) => {
+    return (
+        <div style={{ display: "flex", flexDirection: "row", gap: "1rem" }}>
+            <div>
+                <label htmlFor="x-slider" style={{ marginRight: "1rem" }}>X:</label>
+                <Slider
+                    id="x-slider"
+                    value={x}
+                    onChange={(e) => onXChange(e.value)}
+                    min={-5}
+                    max={3}
+                    style={{ width: "200px" }}
+                />
+                <span style={{ marginLeft: "1rem" }}>{x}</span>
+            </div>
+
+            <div>
+                <label htmlFor="y-text" style={{ marginRight: "1rem" }}>Y:</label>
+                <InputText
+                    id="y-text"
+                    value={y}
+                    onChange={(e) => onYChange(Number(e.target.value))}
+                    style={{ width: "50px" }}
+                />
+            </div>
+
+            <div>
+                <label htmlFor="r-slider" style={{ marginRight: "1rem" }}>R:</label>
+                <Slider
+                    id="r-slider"
+                    value={r}
+                    onChange={(e) => onRChange(e.value)}
+                    min={1}
+                    max={3}
+                    style={{ width: "200px" }}
+                />
+                <span style={{ marginLeft: "1rem" }}>{r}</span>
+            </div>
+        </div>
+    );
+};
 
 export default PointInput;
